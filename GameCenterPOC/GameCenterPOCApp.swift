b6ae56adcbaeee7c24169dfc6df44df0546bb68d
Fernando Sulzbach <f.sulzbach02@gmail.com>
2025-09-02 14:29:12 -0300

    Initial Commit

---
+++ GameCenterPOC/GameCenterPOCApp.swift
@@ -0,0 +1,17 @@
+//
+//  GameCenterPOCApp.swift
+//  GameCenterPOC
+//
+//  Created by Fernando Sulzbach on 02/09/25.
+//
+
+import SwiftUI
+
+@main
+struct GameCenterPOCApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
